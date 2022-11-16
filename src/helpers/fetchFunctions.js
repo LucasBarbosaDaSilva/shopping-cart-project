24b640a0e9f87b69b6e57c121eeb35e921f60408
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2022-11-16 19:25:12 -0300

    Tentando corrigir erro sem parametro

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,11 +3,13 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (selector) => {
-  try {
-    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${selector}`);
-    const json = await response.json();
-    return json.results;
-  } catch (error) {
-    throw new Error('Termo de busca não informado');
+  if (selector !== undefined) {
+    try {
+      const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${selector}`);
+      const json = await response.json();
+      return json.results;
+    } catch (error) {
+      throw new Error('Termo de busca não informado');
+    }
   }
 };
