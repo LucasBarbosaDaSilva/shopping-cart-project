450dca08457fe2b537b0d7aa07ff070aaaa9c89d
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2022-11-21 15:07:00 -0300

    Feito requisito 6 e 7, adicionando API de busca do produto por ID

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,5 +1,11 @@
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (productID) => {
+  if (productID === undefined) {
+    throw new Error('ID não informado');
+  } else {
+    const response = await fetch(`https://api.mercadolibre.com/items/${productID}`);
+    const json = await response.json();
+    return json;
+  }
 };
 
 export const fetchProductsList = async (selector) => {
--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -2,9 +2,14 @@ import './mocks/fetchSimulator';
 import { fetchProduct } from '../helpers/fetchFunctions';
 import product from './mocks/product';
 
-// implemente seus testes aqui
-describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+describe('1 - Teste a função fetchProduct', () => {
+  it('Testa se fetchProduct é uma função', () => {
+   expect(fetchProduct).toBeInstanceOf(Function);
   });
+  it('Testa a função fetchProduct com sem argumento retorna erro com mensagem', () => {
+   expect(async () => {await fetchProduct()}).rejects.toThrow('ID não informado');
+  } );
+  it('Testa a função fetchProduct com argumento retorna resultado esperado', async () => {
+    expect(await fetchProduct('MLB1405519561')).toMatchObject(product);
+   } )
 });
